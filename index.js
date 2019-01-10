35f1ab13fa55877230bf6762a5acaba0050dd7d7
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-10 21:12:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,4 @@
 function toHappyHolidays (name) {
   return 'Happy Holidays, ${name}!'
 }
-function happyHolidayTo (holiday, name) {
-  return "Happy " + `${holiday}` + ", " + `${name}` + "!";	  return `Happy ${holiday}, ${name}!`;
-}	}
 
